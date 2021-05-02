d3cb945e057a11fd595092b99c1130008cb8ed25
Chanvin <chanvinxiao@163.com>
2021-05-02 21:27:29 +0800

    todo
    add top level todo process

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Todo from './components/todo';
 import Subtask from './components/subtask';
+const be = `http://${window.location.host.replace(3000, 3001)}/`;
 
 function App() {
-  const [todoList, setTodoList] = useState([{id: 0}]);
+  const [todoList, setTodoList] = useState([]);
   const [extend, setExtend] = useState(0);
+  const getTodo = async () => {
+    let result = await fetch(be).then(response => response.json());
+    if(!result.error) {
+      setTodoList(result.data);
+    }
+  }
+  const updateTodo = async e => {
+    let id = Number(e.target.name.replace(/todo-/, ''));
+    let status = e.target.checked ? 'completed' : 'pending';
+    let result = await fetch(be, { 
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `id=${id}&status=${status}`,
+    })
+      .then(response => response.json());
+    if(result.error) {
+      alert('System error, Please try again later');
+    } else {
+      getTodo();
+    }
+  }
+
+  useEffect(() => {
+    getTodo();
+  // eslint-disable-next-line
+  }, [])
 
   return (
     <div>
       <h1>Todo App</h1>
-      <Todo />
+      <Todo getTodo={getTodo} be={be} />
       <ul>
         {todoList.map(v => (
           <li key={v.id}>
             <dl>
-              <dt>{v.title}</dt>
+              <dt><input name={`todo-${v.id}`} type="checkbox" checked={'completed' === v.status} onChange={updateTodo} />{v.title}</dt>
               {extend === v.id ? <dd><Subtask /></dd> : null}
             </dl>
           </li>
--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,7 +1,50 @@
-function Todo() {
+import { useState, useRef } from 'react';
+
+function Todo(props) {
+  const [message, setMessage] = useState({});
+  const title = useRef();
+  const addTodo = async () => {
+    if(!title.current.value) {
+      setMessage({
+        type: 'error',
+        content: 'Please fill in title first',
+      });
+      return;
+    }
+    let result = await fetch(props.be, { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `title=${title.current.value}`,
+    })
+      .then(response => response.json());
+    if(result.error) {
+      setMessage({
+        type: 'failure',
+        content: 'Add failure, Please try again later',
+      });
+    } else {
+      setMessage({
+        type: 'success',
+        content: 'Add success'
+      });
+      setTimeout(() => {
+        setMessage({});
+      }, 1000);
+      props.getTodo();
+    }
+  }
+  const handleKeyDown = e => {
+    if(13 === e.keyCode) {
+      addTodo();
+    }
+  }
+
   return (
     <div>
-      Todo
+      <p type={message.type}>{message.content ? <span role="message">{message.content}</span> : null}</p>
+      <input placeholder="What to do?" ref={title} onKeyDown={handleKeyDown} /> <button onClick={addTodo}>New List</button>
     </div>
   );
 }
